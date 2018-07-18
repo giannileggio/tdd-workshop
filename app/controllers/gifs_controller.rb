e4848992bcadf93e5bb3fffbe6478614729a621a
Gianni Leggio <giannileggio@gmail.com>
2018-07-18 15:30:29 +0100

    Gifs controller renders json

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -3,8 +3,8 @@ class GifsController < ApplicationController
     if params[:q].blank?
       render json: {}, status: 400
     else
-      GiphyClient.new.search(params[:q])
-      render json: {}, status: 200
+      gifs = GiphyClient.new.search(params[:q])
+      render json: gifs, status: 200
     end
   end
 end
--- spec/controllers/gifs_controller_spec.rb
+++ spec/controllers/gifs_controller_spec.rb
@@ -6,17 +6,17 @@ end
 
 RSpec.describe GifsController do
   describe '#index' do
-    let(:client) { instance_double(GiphyClient, search: true) }
+    let(:client) { instance_double(GiphyClient, search: [1, 2]) }
 
     before do
       allow(GiphyClient).to receive(:new).and_return(client)
+      get :index, params: { q: 'dublin' }
     end
 
     context 'when parameters are valid' do
-      it 'calls the giphy client' do
-        get :index, params: { q: 'dublin' }
-        expect(client).to have_received(:search).with('dublin')
-      end
+      it { expect(client).to have_received(:search).with('dublin') }
+
+      it { expect(JSON.parse(response.body)).to eq [1, 2] }
     end
 
     context 'when parameters are not valid' do
