c61ebb7fd0cab6647df4ed6f95fe123672c7576b
Gianni Leggio <giannileggio@gmail.com>
2018-07-18 15:04:43 +0100

    Cover case when q is null

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,5 +1,9 @@
 class GifsController < ApplicationController
   def index
-    render json: {}
+    if params[:q].blank?
+      render json: {}, status: 400
+    else
+      render json: {}, status: 200
+    end
   end
 end
--- spec/requests/gifs_request_spec.rb
+++ spec/requests/gifs_request_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe '/gifs' do
   describe 'GET /gifs' do
-    it 'returns response code 200' do
-      get '/gifs?q=friday'
-      expect(response.code).to eq '200'
+    context 'when parameters are valid' do
+      it 'returns response code 200' do
+        get '/gifs?q=friday'
+        expect(response.code).to eq '200'
+      end
+    end
+
+    context 'when q is empty' do
+      it 'returns response code 400' do
+        get '/gifs?q='
+        expect(response.code).to eq '400'
+      end
+    end
+
+    context 'when there is no parameter' do
     end
   end
 end
